16c2a87b540f7a6fa02465fc110145c48d291e56
okerekechinweotito <you@example.com>
2021-06-16 14:47:44 +0100

    add icon and loadingpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
-import './App.css';
+import { useState, useEffect } from "react";
+import LoadingLogo from "../src/helper/loadingLogo/loadingLogo.jsx";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      return setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      {loading === false ? (
+        <div className="App">
+          <h1>Coming Soon</h1>
+          <h1>Coming Soon</h1>
+          <h1>Coming Soon</h1>
+          <h1>Coming Soon</h1>
+          <h1>Coming Soon</h1>
+          <h1>Coming Soon</h1>
+        </div>
+      ) : (
+        <LoadingLogo />
+      )}
+    </>
   );
 }
 
